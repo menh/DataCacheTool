b5d2fc24f639310d3d05908acdd1628f9ceccd0e
menhu <872204778@qq.com>
2018-08-07 18:32:57 +0800

    data cache tool

--- DATA_CACHE_CPP/include/datacache.h
+++ DATA_CACHE_CPP/include/datacache.h
@@ -1,7 +1,10 @@
 #if !defined __data_cache_h__
 #define __data_cache_h__
-template <typename CDataItem>
 
+/*
+Data Index Change Event, not support delete for avoid Halloween question
+*/
+template <typename CDataItem>
 class CIndexEvent 
 { 
 public:
@@ -11,123 +14,6 @@ public:
   virtual int Clear() = 0;                                                 /*数据事件*/
 };
 
-template<typename CDataItem>
-class CDataCache
-{ 
-public:
-  CDataCache(): m_iItemNum(0)
-  {
-    m_pclLoadHandler = 0x0;
-  }
-  virtual ~CDataCache()
-  {
-    Free();
-  }
-  
-  int Append(CDataItem &p_refclDataItem)
-  { 
-    int iRetCode = -1;
-    /*检查能否增加*/
-    typename list<CIndexEvent<CDataItem>* > ::iterator it;
-    bool bCanAdd = true;
-    for(it = m_lstIndexEvent.begin(); it != m_lstIndexEvent.end(); it++)
-    { 
-      CIndexEvent<CDataItem>* pIndexEvent = *it;
-      bCanAdd = pIndexEvent->CanAdd();  
-      if(!bCanAdd)
-      { 
-        break;
-      }
-    }
-    if(!bCanAdd)
-    { 
-      iRetCode = -1;
-    } 
-    else
-    {
-      m_clItems.push_back(p_refclDataItem);
-      typename list<CIndexEvent<CDataItem>* > ::iterator it;
-      for(it = m_lstIndexEvent.begin(); it != m_lstIndexEvent.end(); it++)
-      {
-        CIndexEvent<CDataItem>* pIndexEvent = *it;
-        pIndexEvent->Add(p_refclDataItem, m_iItemNum);
-      }
-      iRetCode = m_iItemNum;  
-      m_iItemNum++;
-    }
-    return iRetCode;
-  }
-
-  int Count(){ return m_iItemNum; }
-
-  void Update(int p_iIndex, CDataItem & p_refclDataItem)
-  { 
-    if(p_iIndex <0 || p_iIndex >= m_iItemNum)
-    { 
-      throw out_of_range("索引下标越界");
-    } 
-    bool bCanUpdate = true;
-    typename list<CIndexEvent<CDataItem>* > ::iterator it;
-    for(it = m_lstIndexEvent.begin(); it != m_lstIndexEvent.end(); it++)
-    { 
-      CIndexEvent<CDataItem>* pIndexEvent = *it;
-      bCanUpdate = pIndexEvent->CanUpdate(m_clItems[p_iIndex], p_refclDataItem);
-      if(!bCanUpdate)
-      { 
-        throw logic_error("不到修改索引字段");
-      } 
-    } 
-    m_clItems[p_iIndex] = p_refclDataItem;  
-  } 
-  
-  const CDataItem & operator[](int p_iIndex) const
-  { 
-    if (p_iIndex <0 || p_iIndex >= m_iItemNum)
-    {
-      throw out_of_range("索引下标越界");
-    }
-    return m_clItems[p_iIndex];
-  }
-  void Clear()
-  {
-    Free();
-  }
-  void RegisterNotify(CIndexEvent<CDataItem> * p_pclIndexEvent)
-  { 
-    if(p_pclIndexEvent != 0)
-    {
-      m_lstIndexEvent.push_back(p_pclIndexEvent);
-    }
-  }
-  void UnRegisterNotify(CIndexEvent<CDataItem> * p_pclIndexEvent)
-  {
-    if(p_pclIndexEvent != 0)
-    { 
-      m_lstIndexEvent.remove(p_pclIndexEvent);
-    } 
-  }
-  void SetLoadHandler(CDataCacheLoadHandler * p_pclLoadHandler)
-  {
-    if(p_pclLoadHandler)
-    { 
-      m_pclLoadHandler = p_pclLoadHandler;
-    } 
-  }
-  int Load(void);
-  int Load(const char* p_pszSql);
-protected:
-  void Free()
-  { 
-    m_clItems.clear();
-    m_iItemNum = 0;
-  }
-
-protected:
-  vector<CDataItem> m_clItems;
-  int m_iItemNum;
-  list<CIndexEvent<CDataItem>* > m_lstIndexEvent;
-  CDataCacheLoadHandler * m_pclLoadHandler;
-};
 
 /*
 Unique Index模板类定义
@@ -150,22 +36,15 @@ private:
     } 
     virtual int Add(CDataItem &newValue, int iPos)
     { 
-      m_pclIndex->UpdateIndex(newValue, iPos);
       return 0;
     }
     virtual bool CanUpdate(CDataItem &oldValue, CDataItem &newValue)
     { 
-      string szOldKey = m_pclIndex->SearchKey(oldValue);
-      string szNewKey = m_pclIndex->SearchKey(newValue);
-      return (szOldKey == szNewKey);
+      return true;
     } 
     virtual int Clear()
     {
       return -1;    
-    }
-    void SetParent(CDataCacheUniqueIndex<CDataIndex, CDataItem, CQueryCondtion> * p_pclIndex)
-    { 
-      m_pclIndex = p_pclIndex;
     }
   private:
     CDataCacheUniqueIndex<CDataIndex, CDataItem, CQueryCondtion> * m_pclIndex;
@@ -297,4 +176,53 @@ private:
   CUniqueIndexEvent m_clUniqueIndexEvent;
   string m_szIndexName;
 };
+
+/*
+进度控制
+*/
+class CDataCacheLoadHandler
+{ 
+public:
+  void virtual NotifyLoading(int iStep, const char * p_pszMsg) = 0;
+};
+
+template<typename CDataItem>
+class CDataCache
+{ 
+public:
+	CDataCache(): m_iItemNum(0)
+  	{
+		m_pclLoadHandler = 0x00;
+	}
+	
+	virtual ~CDataCache()
+  	{
+		Free();
+	}
+	
+	int Count()
+	{
+		return m_iItemNum;
+	}
+	
+  	void Clear()
+  	{
+    	Free();
+  	}
+	    
+  	int Load(void);
+  	
+	int Load(const char* p_pszSql);
+protected:
+	void Free()
+	{ 
+		m_clItems.clear();
+		m_iItemNum = 0;
+	}
+protected:
+	vector<CDataItem> m_clItems;
+	int m_iItemNum;
+	CDataCacheLoadHandler * m_pclLoadHandler;
+};
+
 #endif
---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <datacache.h> 
+using namespace std;
+int main()
+{
+	
+	return 0;
+}
